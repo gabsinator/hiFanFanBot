63c0dfab5754b613e3382200be3156688e13ec33
gabsinator <gabsinator53@gmail.com>
2022-05-05 20:39:50 +0200

    saves last ids now in gitHub resp

--- hifanfan_bot.py
+++ hifanfan_bot.py
@@ -2,6 +2,7 @@
 import time
 import tweepy
 import os
+from github import Github
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,16 +12,14 @@ access_token = os.getenv("access_token")
 access_token_secret = os.getenv("access_token_secret")
 bearer_token = os.getenv("bearer_token")
 
-last_tweet = int(os.getenv("last_tweet"))
-
+github_token = os.getenv("github_token")
 
 
 exclude_list = ["retweets", "replies"]
 tweet = "Hello World"
-FILE_NAME = "last.txt"
+FILE_NAME = "/last.txt"
 USER_ID = 1337072139216670723 #id for fanfan
 #735765478845489157 #id for gabsinatorrr
-
 
 
 
@@ -34,6 +33,9 @@ client = tweepy.Client(
     access_token_secret
 )
 
+github = Github(github_token)
+repo = github.get_user().get_repo("hiFanFanBot")
+
 def get_user_id(username):
     response = client.get_user(username=username)
     return response
@@ -41,17 +43,19 @@ def get_user_id(username):
 
 #read ID of last tweet the bot has replied to
 def read_last(FILE_NAME):
-    file = open(FILE_NAME, "r")
-    last_id = int(file.read().strip())
-    file.close()
 
-    return last_id
+    print("reading ID...")
+    file = repo.get_contents(FILE_NAME)
+    last_id = file.decoded_content.decode()
+    
+    return int(last_id)
 
 #store ID of the last tweet the bot replied to
-def store_last(FILE_NAME, last_id):
-    file = open(FILE_NAME, "w")
-    file.write(str(last_id))
-    file.close()
+def store_last(last_id):
+
+    print("saving ID...")
+    contents = repo.get_contents(FILE_NAME)
+    repo.update_file(contents.path, "more tests", str(last_id), contents.sha, branch="master")
 
     return
 
@@ -59,9 +63,6 @@ def reply():
     tweets = client.get_users_tweets(id=USER_ID, exclude= exclude_list, since_id= read_last(FILE_NAME= FILE_NAME))
 
     count = 0
-    #os.environ["my_var"] = "1"
-    print(int(os.getenv("my_var")))
-
 
     try:
         for tweet in reversed(tweets.data):
@@ -72,7 +73,7 @@ def reply():
             client.create_tweet(text= "hi fanfan", in_reply_to_tweet_id= tweet_id)
             count += 1
 
-            store_last(FILE_NAME= FILE_NAME, last_id= tweet_id)
+            store_last(last_id= tweet_id)
 
         print("replied to {0} tweets".format(count))
 
